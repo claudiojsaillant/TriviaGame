15426a6b1c8943fe74f5f90cba121032bed5ff79
Cloud <claudiojsaillant@gmail.com>
2019-06-27 12:39:05 -0400

    adding boiler plates, setting up files / made timer function

---
+++ assets/javascript/script.js
@@ -0,0 +1,59 @@
+
+
+
+
+
+
+
+
+
+$('#startbutton').on('click', function hey(){
+    $('#startbutton').hide();
+    run();
+
+})
+
+    var number = 30;
+
+    //  Variable that will hold our interval ID when we execute
+    //  the "run" function
+    var intervalId;
+
+    //  Clearing the intervalId prior to setting our new intervalId will not allow multiple instances.
+    function run() {
+      clearInterval(intervalId);
+      intervalId = setInterval(decrement, 1000);
+    }
+
+    //  The decrement function.
+    function decrement() {
+
+      //  Decrease number by one.
+      number--;
+
+      //  Show the number in the #show-number tag.
+      $("#timer").html("<h2>" + number + "</h2>");
+
+
+      //  Once number hits zero...
+      if (number === 0) {
+
+        //  ...run the stop function.
+        stop();
+
+        //  Alert the user that time is up.
+        alert("Time Up!");
+      }
+    }
+
+    //  The stop function
+    function stop() {
+
+      //  Clears our intervalId
+      //  We just pass the name of the interval
+      //  to the clearInterval function.
+      clearInterval(intervalId);
+    }
+
+
+    
